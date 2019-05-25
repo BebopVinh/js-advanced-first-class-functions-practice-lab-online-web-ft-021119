f374924401787f76da5288cfddf6aa79f75416cb
bebopvinh <vinhdo07@gmail.com>
2019-05-24 22:41:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+// forEach()
+const nameCallback = function (el) {
+   console.log(el.name)
+}
+
+function logDriverNames(array) {
+   array.forEach(nameCallback)
+}
+
+
+function logDriversByHometown(array, hometown) {
+   array.forEach(function (el) {
+      if (el.hometown === hometown) {
+         nameCallback(el)
+      }
+   })
+}
+
+// sort()
+
+function driversByRevenue(array) {
+   const sortedDrivers = [ ...array]
+   sortedDrivers.sort(function (a, b) {
+      return a.revenue - b.revenue
+   })
+
+   return sortedDrivers
+}
+
+function driversByName(array) {
+   const sortedDrivers = [ ...array]
+   return sortedDrivers.sort( function (a,b) {
+      return a.name.localeCompare(b.name)
+   })
+}
+
+function totalRevenue(array) {
+   const totalRevenue = array.reduce( function (sum, el) {
+      return sum += el.revenue
+   }, 0)
+   return totalRevenue
+}
+
+function averageRevenue(array) {
+   const total = totalRevenue(array)
+   return total / array.length
+}
